de3c034e347f82cb264f79a44e4b2aca42318e74
malagantisatish <satishvarma8247@gmail.com>
2025-02-24 23:04:28 +0530

    started building full stack app

---
+++ backend/src/controllers/auth.controllers.js
@@ -0,0 +1,11 @@
+export const signup = (reqest,response)=>{
+    response.send("signup route")
+}
+
+export const login = (reqest,response)=>{
+    response.send("login route")
+}
+
+export const logout = (reqest,response)=>{
+    response.send("logout route")
+}
