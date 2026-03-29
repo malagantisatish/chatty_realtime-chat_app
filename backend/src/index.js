802fda54a287bfb6b6ecb99cfe962047a0514983
malagantisatish <satishvarma8247@gmail.com>
2026-03-29 17:11:29 +0530

    fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,39 +5,39 @@ import authRoutes from "./routes/auth.route.js"
 import messageRoute from "./routes/messageRoute.route.js"
 import cookieParser from "cookie-parser"
 import cors from "cors";
-import {app,server} from "./lib/socket.js"
+import { app, server } from "./lib/socket.js"
 import path from "path";
 
 dotenv.config() // for accessing .env variables we need to import and config()
 
-const PORT = process.env.PORT 
+const PORT = process.env.PORT
 const _dirname = path.resolve()
 
 app.use(express.json()) // for destructuring json we need to do this
 app.use(cookieParser())
 
 app.use(cors({
-    origin:"http://localhost:3000",
-    credentials:true
+    origin: "http://localhost:3000",
+    credentials: true
 }
 ))
 
 
 
 
-app.use("/api/auth",authRoutes)
-app.use("/api/messages",messageRoute)
+app.use("/api/auth", authRoutes)
+app.use("/api/messages", messageRoute)
 
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(_dirname,"../fronted/dist")));
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(_dirname, "../frontend/dist")));
 
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(_dirname,"../frontend","dist","index.html"));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(_dirname, "../frontend", "dist", "index.html"));
     })
 }
 
 
-server.listen(PORT,()=>{
+server.listen(PORT, () => {
     console.log(`server is running on port ${PORT}`)
     connectDB()
 })
